02a60542d13489756530277d7a64be319846e82a
Mishmore <michellemore28@gmail.com>
2017-05-02 17:39:05 -0500

    validacion con boton

--- assets/js/register.js
+++ assets/js/register.js
@@ -51,3 +51,34 @@ city.addEventListener("blur", function() {
     document.getElementById("city-field").classList.remove("incorrect");
   }
 });
+
+//validacioón con botón
+document.getElementById("submit").addEventListener("click", function() {
+  var phone = document.getElementById("phone");
+    if (phone.value == null || phone.value.length == 0 || ( !(/^\d{9}$/.test(phone.value) )) ) {
+      document.getElementById("phone-field").classList.add("incorrect");
+    }  else {
+      document.getElementById("phone-field").classList.remove("incorrect");
+    }
+
+  var userName = document.getElementById("name");
+    if (userName.value == null || userName.value.length == 0 ) {
+      document.getElementById("name-field").classList.add("incorrect");
+    }  else {
+      document.getElementById("name-field").classList.remove("incorrect");
+    }
+
+  var email = document.getElementById("email");
+    if (email.value == null || email.value.length == 0 || ( !(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value))) ) {
+      document.getElementById("email-field").classList.add("incorrect");
+    }  else {
+      document.getElementById("email-field").classList.remove("incorrect");
+    }
+
+  var city = document.getElementById("city");
+    if (city.value == null || city.value.length == 0 ) {
+      document.getElementById("city-field").classList.add("incorrect");
+    }  else {
+      document.getElementById("city-field").classList.remove("incorrect");
+    }
+});
